eeddb86d1f165716927318169c819fe2c77e2080
Michael Smith <michaelsmith.a@gmail.com>
2020-11-28 08:59:10 -0800

    added answers for stack

--- exercises/Stack.js
+++ exercises/Stack.js
@@ -5,18 +5,24 @@ class Stack {
     }
 
     // Add an item to the top of the stack
-    push() {
-
+    push(value) {
+        this.stack.push(value);
+        this.size++;
     }
 
     // Remove an item from the top of the stack and return it. Return `null` if the stack is empty.
     pop() {
-
+        if (this.stack.length == 0)
+            return null;
+            
+        var value = this.stack.pop();
+        this.size--;
+        return value;
     }
 
     // Return the item on top of the stack
     peek() {
-
+        return this.stack[this.stack.length - 1];
     }
 
 }
